97052293e4a771251cf0546c9696195a9eeb0490
Shashike Dissanayake <shashikedissanayake@gmail.com>
2018-11-16 22:09:11 +0530

    Removed worker thread created section

--- Fractels/src/Main.java
+++ Fractels/src/Main.java
@@ -70,20 +70,6 @@ public class Main {
         JFrame frame = new JFrame("Fractals");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(800,800));
-//        frame.setLayout(new GridLayout(2,2));
-
-//        Thread thread1 = new Thread(new Worker(400,400,calculation,0,0,frame,1));
-//        Thread thread2 = new Thread(new Worker(400,400,calculation,0,400,frame,2));
-//        Thread thread3 = new Thread(new Worker(400,400,calculation,400,0,frame,3));
-//        Thread thread4 = new Thread(new Worker(400,400,calculation,400,400,frame,4));
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
-//        thread4.start();
-//        thread1.join();
-//        thread2.join();
-//        thread3.join();
-//        thread4.join();
         drawer = new Drawer(calculation,800,800,0,0,1);
         frame.setContentPane(drawer);
         frame.pack();
